eda96468dbd76845b736dd6db90939f1604f28ef
yashul117 <yashultyagi11@gmail.com>
2022-03-03 12:46:57 +0530

    Updated main code

--- main.py
+++ main.py
@@ -5,8 +5,7 @@
 import pywhatkit as kit
 import cv2
 
-kit.text_to_handwriting("Scarcity means that there are fewer resources than are needed to fill human wants and needs. These resources can come from the land, labor resources or capital resources. Keep reading for scarcity examples that you may see on a global economic level or in your everyday life.", save_to="handwriting.png")
-img = cv2.imread("handwriting.png")
+kit.text_to_handwriting("Hey guys, this is Yashul Tyagi. I am a software developer from Ghaziabad, Uttar Pradesh, India.png")
 cv2.imshow("Text to Handwriting", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
